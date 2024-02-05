714ea99d76598125a371c2331fb52db6be0331fc
Dimitar Donkov <m17k018@gmail.com>
2024-02-05 18:09:59 +0200

    Add navigation to templates

--- src/views.py
+++ src/views.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template, current_app
 import os
 
-views = Blueprint("views", __name__, template_folder='templates')
+views = Blueprint("views", __name__, template_folder="../templates/")
 
 @views.route('/')
 def home():
-    # return f"{current_app.template_folder}"
     return render_template("home.html")

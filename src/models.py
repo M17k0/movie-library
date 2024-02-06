5a3d5e656b13be2186e2fef8b33e3f653135e8d7
Dimitar Donkov <m17k018@gmail.com>
2024-02-06 15:26:22 +0200

    Create database structure

---
+++ src/models.py
@@ -0,0 +1,46 @@
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+from .setup import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True)
+    password_hash = db.Column(db.String(64))
+    password_salt = db.Column(db.String(8))
+
+    watched_movies = db.relationship("Watched")
+    reviews = db.relationship("Review")
+    watchlist = db.relationship("Watchlist")
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tmdb_id = db.Column(db.Integer)
+    title = db.Column(db.String(256))
+    release_date = db.Column(db.DateTime)
+
+    movie_genres = db.relationship("MovieGenre")
+
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tmdb_id = db.Column(db.Integer)
+    name = db.Column(db.String(64))
+
+    movie_genres = db.relationship("MovieGenre")
+
+class MovieGenre(db.Model):
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"), primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"), primary_key=True)
+
+class Watched(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    rating = db.Column(db.Float)
+    review = db.Column(db.Text)
+    date_watched = db.Column(db.DateTime, default=func.now())
+
+class Watchlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,35 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
 from .views import views
 from .auth import auth
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "very secret key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     app.template_folder = 'templates'
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from . import models
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    db_path = path.join("..", "instance", DB_NAME)
+    if not path.exists(db_path):
+        with app.app_context():
+            db.create_all()
+            print("Database Created")
+        return
+    print("Database already exists")

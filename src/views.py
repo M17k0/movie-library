b2fa5eaff89c8a4075af3eb92299f454517e8100
Dimitar Donkov <m17k018@gmail.com>
2024-02-07 23:45:16 +0200

    Fix default page redirection

--- src/views.py
+++ src/views.py
@@ -4,7 +4,14 @@ from flask_login import current_user, login_required
 
 views = Blueprint("views", __name__, template_folder="../templates/")
 
-@views.route('/')
+@views.route("/")
+def main_route():
+    if current_user.is_authenticated:
+         return render_template("home.html", user=current_user)
+    else:
+         return render_template("login.html", user=current_user)
+
+@views.route('/home')
 @login_required
 def home():
     return render_template("home.html", user=current_user)

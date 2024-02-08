5fd27a122cd1e90d8fb78b2234f04c3dc307b9f4
Dimitar Donkov <m17k018@gmail.com>
2024-02-08 18:21:40 +0200

    Add watched and watchlist

--- src/views.py
+++ src/views.py
@@ -4,14 +4,28 @@ from flask_login import current_user, login_required
 
 views = Blueprint("views", __name__, template_folder="../templates/")
 
-@views.route("/")
-def main_route():
-    if current_user.is_authenticated:
-         return render_template("home.html", user=current_user)
-    else:
-         return render_template("login.html", user=current_user)
+# @views.route("/", methods=["GET", "POST"])
+# def main_route():
+#     if current_user.is_authenticated:
+#          return render_template("home.html", user=current_user)
+#     else:
+#          return render_template("login.html", user=current_user)
 
-@views.route('/home')
+@views.route('/watched')
+def watched():
+    from .models import Watched
+
+    watched_movies = Watched.query.all()
+    return render_template('watched.html', user=current_user, watched_movies=watched_movies)
+
+@views.route('/watchlist')
+def watchlist():
+    from .models import Watchlist
+
+    watchlist = Watchlist.query.all()
+    return render_template('watchlist.html', user=current_user, watched_movies=watchlist)
+
+@views.route('/')
 @login_required
 def home():
     return render_template("home.html", user=current_user)
